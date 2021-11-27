86cba7a2b47e1c54fada4e4ee9a01dbae6330d60
Abdur Rahman <khalil.ahmad1547@gmail.com>
2021-11-27 14:02:25 +0500

    Database design, setup add new user quires are added.

---
+++ Database/add_user.sql
@@ -0,0 +1,7 @@
+USE mydb;
+
+INSERT INTO User(First_Name, Last_Name, E_mail_ID) VALUES('Abdur', 'Rahman', 'kahlil.ahmad1547@gmail.com');
+
+-- INSERT INTO User(First_Name, Last_Name, E_mail_ID) VALUES('Amir', 'Malik', 'amir.malik@gmail.com');
+
+-- SELECT * FROM USER;
